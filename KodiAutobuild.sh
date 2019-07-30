8d49a7ed30ed197508ecb773a179a5a541a57c87
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-07-30 13:44:54 +0000

    Create KodiAutobuild.sh

---
+++ KodiAutobuild.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+mkdir kodibuild;cd kodibuild;wget https://raw.githubusercontent.com/AppImage/AppImages/master/pkg2appimage; mv pkg2appimage pkg2appimage.sh;chmod +x pkg2appimage.sh;wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/kodi-Nightly-xenial.yml ; ARCH=x86_64 ./pkg2appimage.sh kodi-Nightly-xenial.yml
