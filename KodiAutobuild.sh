13dbc1599aea0a46b324baeb5a03914d8241962c
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-07-31 09:57:43 +0000

    Update KodiAutobuild.sh

--- KodiAutobuild.sh
+++ KodiAutobuild.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-mkdir kodibuild;cd kodibuild;wget https://raw.githubusercontent.com/AppImage/AppImages/master/pkg2appimage; mv pkg2appimage pkg2appimage.sh;chmod +x pkg2appimage.sh;wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/kodi-Nightly-xenial.yml ; ARCH=x86_64 ./pkg2appimage.sh kodi-Nightly-xenial.yml
+mkdir kodibuild;cd kodibuild;wget https://raw.githubusercontent.com/AppImage/AppImages/master/pkg2appimage; mv pkg2appimage pkg2appimage.sh;chmod +x pkg2appimage.sh;wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/Kodi-stableXenial.yml ; ARCH=x86_64 ./pkg2appimage.sh Kodi-stableXenial.yml
