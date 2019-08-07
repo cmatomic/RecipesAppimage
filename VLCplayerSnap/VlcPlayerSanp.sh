d5d5418cb20fc64eb6299181a289ea0fdcf6940f
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-08-07 21:37:24 +0000

    Update VlcPlayerSanp.sh

--- VLCplayerSnap/VlcPlayerSanp.sh
+++ VLCplayerSnap/VlcPlayerSanp.sh
@@ -1,10 +1,22 @@
-#!/usr/bin/env bash
-
-# env
+#!/bin/sh
+# run in Ubunu xenial 16.04
 
 
 mkdir VLCsnapBuild
+mkdir out
 cd VLCsnapBuild
 snap download vlc
 file-roller -h vlc_*
 mv vlc_* mv VlcPlayer-snap.AppDir
+cd VlcPlayer-snap.AppDir
+wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/AppRun
+wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/vlc.desktop
+wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/vlc.png
+wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/vlc.wrapper
+wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/vlc.appdata.xml
+chmod +x AppRun
+rm /usr/bin/vlc.wrapper
+rm /usr/share/metainfo/vlc.appdata.xml
+chmod +x vlc.wrapper
+mv vlc.wrapper /usr/bin
+mv vlc.appdata.xml /usr/share/metainfo
