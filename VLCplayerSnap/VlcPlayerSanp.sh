79a1e8e929fcd3665ff455eb430852376099c75f
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-08-08 04:24:46 +0000

    Update VlcPlayerSanp.sh

--- VLCplayerSnap/VlcPlayerSanp.sh
+++ VLCplayerSnap/VlcPlayerSanp.sh
@@ -6,24 +6,24 @@ mkdir VLCsnapBuild
 cd VLCsnapBuild
 mkdir out
 snap download vlc
-file-roller -h vlc_*
-mv vlc_*.snap mv VlcPlayer-snap.AppDir
-cd VlcPlayer-snap.AppDir
+file-roller -h vlc_*.snap
+mv vlc_*/ VlcPlayerSnap.AppDir/
+cd VlcPlayerSnap.AppDir
 wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/AppRun
 wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/vlc.desktop
 wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/vlc.png
 wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/vlc.wrapper
 wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/vlc.appdata.xml
 chmod +x AppRun
-rm /usr/bin/vlc.wrapper
-rm /usr/share/metainfo/vlc.appdata.xml
+rm  /VLCsnapBuild/VlcPlayerSnap.AppDir/usr/bin/vlc.wrapper
+rm /VLCsnapBuild/VlcPlayerSnap.AppDir/usr/share/metainfo/vlc.appdata.xml
 chmod +x vlc.wrapper
-mv vlc.wrapper /usr/bin
-mv vlc.appdata.xml /usr/share/metainfo
+mv vlc.wrapper/VLCsnapBuild/VlcPlayerSnap.AppDir/usr/bin/
+mv vlc.appdata.xml /VLCsnapBuild/VlcPlayerSnap.AppDir/usr/share/metainfo/
 cd ..
 wget https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage
 chmod +x *.AppImage
-./appimagetool-x86_64.AppImage VlcPlayer-snap.AppDir
-mv VlcPlayer-snap.AppImage /out
+./appimagetool-x86_64.AppImage VlcPlayerSnap.AppDir
+mv VlcPlayerSnap.AppImage /out
 cd out
-chmod +x VlcPlayer-snap.AppImage
+chmod +x VlcPlayerSnap.AppImage
