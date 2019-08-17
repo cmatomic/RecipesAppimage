2d4fd465b7078cdef1ea7074595817933d0c4ff9
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-08-17 11:59:05 +0000

    Add files via upload

---
+++ handbrake/handbrake.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+apt update
+apt-get install -qq autoconf automake build-essential cmake git libass-dev libbz2-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libharfbuzz-dev libjansson-dev liblzma-dev libmp3lame-dev libogg-dev libopus-dev libsamplerate-dev libspeex-dev libtheora-dev libtool libtool-bin libvorbis-dev libx264-dev libxml2-dev m4 make nasm patch pkg-config python tar yasm zlib1g-dev intltool libappindicator-dev libdbus-glib-1-dev libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgtk-3-dev libgudev-1.0-dev libnotify-dev libwebkitgtk-3.0-dev libva-dev libdrm-dev libnuma-dev gstreamer1.0-libav intltool libappindicator-dev libdbus-glib-1-dev libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgtk-3-dev libgudev-1.0-dev libnotify-dev libwebkitgtk-3.0-dev 
+
+
+wget https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.xz
+tar xf *.tar.xz
+cd nasm-*
+./configure
+make j4
+make install
+cd ..
+git clone https://github.com/HandBrake/HandBrake.git && cd HandBrake
+
+./configure --launch-jobs=$(nproc) --launch
+
+make --directory=build install
