5821050ff0fe5a12363bcc7ef01565d2f9fe2a10
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-08-18 17:15:06 +0000

    Add files via upload

---
+++ handbrake/dependDEv.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+wget https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.xz
+tar xf *.tar.xz
+cd nasm-*
+./configure
+make j4
+make install
+cd ..
+wget http://archive.ubuntu.com/ubuntu/pool/main/h/harfbuzz/harfbuzz_1.7.2.orig.tar.bz2
+tar xf *.tar.bz2
+./configure
+make j4
+make install
+cd ..
+exit
