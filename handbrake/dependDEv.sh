1b98901999e41b11c4c8ef545e833bb4568210b9
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-08-19 20:41:06 +0000

    Update dependDEv.sh

--- handbrake/dependDEv.sh
+++ handbrake/dependDEv.sh
@@ -1,60 +1,11 @@
 #!/bin/sh
 
-wget https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.xz
+wget https://www.nasm.us/pub/nasm/releasebuilds/2.13/nasm-2.13.tar.xz
 tar xf *.tar.xz
 cd nasm-*
 ./configure
 make j4
 sudo make install
-cd ..
-
-
-wget http://archive.ubuntu.com/ubuntu/pool/main/g/gobject-introspection/gobject-introspection_1.46.0.orig.tar.xz
-tar xf gobject-introspection_1.46.0.orig.tar.xz
-cd gobject-introspection-1.46.0
-./configure                                                    
-make                                
-sudo make install 
-cd ..
-
-wget http://archive.ubuntu.com/ubuntu/pool/main/a/atk1.0/atk1.0_2.18.0.orig.tar.xz
-tar xf atk1.0_2.18.0.orig.tar.xz
-cd atk-2.18.0
-./configure                                                   
-make                                
-sudo make install                   
-cd ..
-
-wget http://archive.ubuntu.com/ubuntu/pool/main/p/pango1.0/pango1.0_1.38.1.orig.tar.xz
-tar xf pango1.0_1.38.1.orig.tar.xz
-cd pango-1.38.1
-./configure                                                     
-make                                
-sudo make install                   
-cd ..
-
-wget http://archive.ubuntu.com/ubuntu/pool/main/c/cairo/cairo_1.14.6.orig.tar.xz
-tar xf cairo_1.14.6.orig.tar.xz
-cd cairo-1.14.6
-./configure                                                    
-make                                
-sudo make install                   
-cd ..
-
-wget http://archive.ubuntu.com/ubuntu/pool/main/g/gdk-pixbuf/gdk-pixbuf_2.32.2.orig.tar.xz
-tar xf gdk-pixbuf_2.32.2.orig.tar.xz
-cd gdk-pixbuf-2.32.2
-./configure                                                     
-make                                
-sudo make install                   
-cd ..
-
-wget http://archive.ubuntu.com/ubuntu/pool/main/g/gtk+3.0/gtk+3.0_3.22.30.orig.tar.xz
-tar xf gtk+3.0_3.22.30.orig.tar.xz  
-cd gtk+-3.22.30                    
-./configure                                                     
-make                                
-sudo make install                   
 cd ..
 
 wget http://archive.ubuntu.com/ubuntu/pool/main/h/harfbuzz/harfbuzz_1.7.2.orig.tar.bz2
