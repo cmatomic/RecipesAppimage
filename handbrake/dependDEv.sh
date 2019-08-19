90023b448defbe696a49ff8ccec53d0ddb45d8d0
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-08-19 14:45:02 +0000

    Update dependDEv.sh

--- handbrake/dependDEv.sh
+++ handbrake/dependDEv.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-wget http://archive.ubuntu.com/ubuntu/pool/main/g/gobject-introspection/gobject-introspection_1.56.1.orig.tar.xz
-tar xf gobject-introspection_1.56.1.orig.tar.xz
-cd gobject-introspection-1.56.1
-./configure
-chmod +x autogen.sh
-./autogen.sh
-sudo make install
-cd ..
+wget http://archive.ubuntu.com/ubuntu/pool/main/g/gobject-introspection/gobject-introspection_1.46.0.orig.tar.xz
+tar xf gobject-introspection_1.46.0.orig.tar.xz
+cd gobject-introspection-1.46.0
+./configure                         
+cd build                            
+make                                
+sudo make install 
+cd ../../
 wget http://archive.ubuntu.com/ubuntu/pool/main/a/atk1.0/atk1.0_2.18.0.orig.tar.xz
 tar xf atk1.0_2.18.0.orig.tar.xz
 cd atk-2.18.0
@@ -14,7 +14,7 @@ cd atk-2.18.0
 cd build                            
 make                                
 sudo make install                   
-cd ...
+cd ../../
 wget http://archive.ubuntu.com/ubuntu/pool/main/p/pango1.0/pango1.0_1.38.1.orig.tar.xz
 tar xf pango1.0_1.38.1.orig.tar.xz
 cd pango-1.38.1
@@ -22,21 +22,21 @@ cd pango-1.38.1
 cd build                            
 make                                
 sudo make install                   
-cd ...
+cd ../../
 wget http://archive.ubuntu.com/ubuntu/pool/main/c/cairo/cairo_1.14.6.orig.tar.xz
 cd cairo-1.14.6
 ./configure                         
 cd build                            
 make                                
 sudo make install                   
-cd ...
+cd ../../
 wget http://archive.ubuntu.com/ubuntu/pool/main/g/gdk-pixbuf/gdk-pixbuf_2.32.2.orig.tar.xz
 cd gdk-pixbuf-2.32.2
 ./configure                         
 cd build                            
 make                                
 sudo make install                   
-cd ...
+cd ../../
 
 wget http://archive.ubuntu.com/ubuntu/pool/main/g/gtk+3.0/gtk+3.0_3.22.30.orig.tar.xz
 tar xf gtk+3.0_3.22.30.orig.tar.xz  
@@ -45,7 +45,7 @@ cd gtk+-3.22.30
 cd build                            
 make                                
 sudo make install                   
-cd ..
+cd ../../
 
 wget https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.xz
 tar xf *.tar.xz
