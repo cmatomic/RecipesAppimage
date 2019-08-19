cbe6c2a4f81d90e970daa8726574d0ead5caac70
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-08-19 11:17:25 +0000

    Update dependDEv.sh

--- handbrake/dependDEv.sh
+++ handbrake/dependDEv.sh
@@ -1,4 +1,51 @@
 #!/bin/sh
+wget http://archive.ubuntu.com/ubuntu/pool/main/g/gobject-introspection/gobject-introspection_1.56.1.orig.tar.xz
+tar xf gobject-introspection_1.56.1.orig.tar.xz
+cd gobject-introspection-1.56.1
+./configure
+chmod +x autogen.sh
+./autogen.sh
+sudo make install
+cd ..
+wget http://archive.ubuntu.com/ubuntu/pool/main/a/atk1.0/atk1.0_2.18.0.orig.tar.xz
+tar xf atk1.0_2.18.0.orig.tar.xz
+cd atk-2.18.0
+./configure                         
+cd build                            
+make                                
+sudo make install                   
+cd ...
+wget http://archive.ubuntu.com/ubuntu/pool/main/p/pango1.0/pango1.0_1.38.1.orig.tar.xz
+tar xf pango1.0_1.38.1.orig.tar.xz
+cd pango-1.38.1
+./configure                         
+cd build                            
+make                                
+sudo make install                   
+cd ...
+wget http://archive.ubuntu.com/ubuntu/pool/main/c/cairo/cairo_1.14.6.orig.tar.xz
+cd cairo-1.14.6
+./configure                         
+cd build                            
+make                                
+sudo make install                   
+cd ...
+wget http://archive.ubuntu.com/ubuntu/pool/main/g/gdk-pixbuf/gdk-pixbuf_2.32.2.orig.tar.xz
+cd gdk-pixbuf-2.32.2
+./configure                         
+cd build                            
+make                                
+sudo make install                   
+cd ...
+
+wget http://archive.ubuntu.com/ubuntu/pool/main/g/gtk+3.0/gtk+3.0_3.22.30.orig.tar.xz
+tar xf gtk+3.0_3.22.30.orig.tar.xz  # unpack the sources
+cd gtk+-3.22.30                     # change to the toplevel directory
+./configure                         # run the `configure' script
+cd build                            
+make                                # build GTK+
+sudo make install                   # install GTK+
+cd ..
 
 wget https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.xz
 tar xf *.tar.xz
@@ -14,13 +61,5 @@ cd harfbuzz-1.7.2
 chmod +x autogen.sh
 ./autogen.sh
 sudo make install
-cd ..
-wget http://archive.ubuntu.com/ubuntu/pool/main/g/gtk+3.0/gtk+3.0_3.22.30.orig.tar.xz
-tar xf gtk+3.0_3.22.30.orig.tar.xz  # unpack the sources
-cd gtk+-3.22.30                     # change to the toplevel directory
-./configure                         # run the `configure' script
-cd build                            
-make                                # build GTK+
-sudo make install                   # install GTK+
 cd ..
 
