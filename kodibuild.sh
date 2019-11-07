69b6df5628150c426410f1ba101218e52e378545
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-11-07 22:13:54 +0000

    Update kodibuild.sh

--- kodibuild.sh
+++ kodibuild.sh
@@ -18,8 +18,8 @@ mkdir kodi.AppDir
 chmod  755 -R kodi-build
 git clone -b Krypton https://github.com/xbmc/xbmc kodi
 chmod  755 -R kodi
-cd $HOME/kodi-build
-cmake ../kodi -DCMAKE_INSTALL_PREFIX=$HOME/kodi.AppDir
+cd/home/kodi-build
+cmake ../kodi -DCMAKE_INSTALL_PREFIX=/home/kodi.AppDir
 cmake --build . -- VERBOSE=1 -j$(getconf _NPROCESSORS_ONLN)
 
 )
@@ -27,15 +27,16 @@ cmake --build . -- VERBOSE=1 -j$(getconf _NPROCESSORS_ONLN)
 
 (
 
-cd $HOME/kodi
+cd /home/kodi
 make -j$(getconf _NPROCESSORS_ONLN) -C tools/depends/target/binary-addons PREFIX=$HOME/kodi.AppDir ADDONS=" audiodecoder.2sf audiodecoder.asap audiodecoder.dumb audiodecoder.fluidsynth audiodecoder.gme audiodecoder.gsf audiodecoder.modplug audiodecoder.ncsf audiodecoder.nosefart audiodecoder.openmpt audiodecoder.organya audiodecoder.qsf audiodecoder.sidplay audiodecoder.snesapu audiodecoder.ssf audiodecoder.stsound audiodecoder.timidity audiodecoder.upse audiodecoder.vgmstream audiodecoder.wsr audiodsp.freesurround audioencoder.flac audioencoder.lame audioencoder.vorbis audioencoder.wav game.libretro imagedecoder.mpo imagedecoder.raw inputstream.adaptive peripheral.joystick pvr.argustvpvr.demo pvr.dvblink pvr.dvbviewer pvr.filmon pvr.hdhomerun pvr.hts pvr.iptvsimple pvr.mediaportal.tvserver pvr.mythtv pvr.nextpvr pvr.njoy pvr.octonet pvr.pctv pvr.sledovanitv.cz pvr.stalker pvr.teleboy pvr.vbox pvr.vdr.vnsi pvr.vupluspvr.wmc pvr.zattoo screensaver.asteroids screensaver.biogenesis screensaver.greynetic screensaver.matrixtrails screensaver.pingpong screensaver.pyro screensaver.stars vfs.libarchive vfs.nfs vfs.rar vfs.sacd vfs.sft pvisualization.goom visualization.shader toyvisualization.spectrum visualization.waveform "
 
 )
 
 
-cd $HOME
+cd /home
 
-cd kodi-build
+cd /home/kodi-build
+
 wget https://github.com/cmatomic/RecipesAppimage/raw/master/kodi.png
 
 wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/AppRun2
@@ -46,7 +47,16 @@ chmod +x AppRun
 
 wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/kodi.desktop
 
-cd $HOME
+cd /home
+
+wget https://github.com/cmatomic/RecipesAppimage/raw/master/VLCplayerSnap/appimagetool
+
+chmod +x appimagetool
+
+./appimagetool --no-appstream kodi.AppDir
+
+
+
 
 wget https://github.com/cmatomic/RecipesAppimage/raw/master/VLCplayerSnap/appimagetool
 
