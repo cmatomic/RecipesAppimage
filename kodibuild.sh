0120ff753db6d92380618dea1abe37c2fc4ab8c5
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-11-07 21:13:04 +0000

    Update kodibuild.sh

--- kodibuild.sh
+++ kodibuild.sh
@@ -1,33 +1,57 @@
 #!/bin/bash
-# for docker
 apt update
-apt install nano
 apt install python-software-properties software-properties-common
 cp /etc/apt/sources.list /etc/apt/sources.list~
 sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
 apt update
-cd home
-mkdir kodibuild ; chmod 755 kodibuild ; cd kodibuild
-git clone -b Krypton https://github.com/xbmc/xbmc kodi
-cd kodi
 add-apt-repository -s ppa:team-xbmc/xbmc-nightly
 add-apt-repository -s ppa:team-xbmc/xbmc-ppa-build-depends
 apt build-dep kodi
 apt install ccache
 apt install libcec4-dev libfmt3-dev liblircclient-dev  libglew-dev libwayland-dev libxkbcommon-dev waylandpp-dev wayland-protocols libgbm-dev libinput-dev libxkbcommon-dev install doxygen libcap-dev libsndio-dev libmariadbd-dev
-make -C tools/depends/target/crossguid PREFIX=/usr/local
-make -C tools/depends/target/flatbuffers PREFIX=/usr/local
-make -C tools/depends/target/libfmt PREFIX=/usr/local
+
+(
+
+cd home
 mkdir kodi-build
-cd build
-cmake ../kodi -DCMAKE_INSTALL_PREFIX=/usr/local
+mkdir kodi.AppDir
+chmod  755 -R kodi-build
+git clone -b Krypton https://github.com/xbmc/xbmc kodi
+chmod  755 -R kodi
+cd $HOME/kodi-build
+cmake ../kodi -DCMAKE_INSTALL_PREFIX=$HOME/kodi.AppDir
 cmake --build . -- VERBOSE=1 -j$(getconf _NPROCESSORS_ONLN)
-make -j$(getconf _NPROCESSORS_ONLN) -C tools/depends/target/binary-addons PREFIX=/usr/local
-cd ..
-mv kodi-build kodi.AppDir
+
+)
+
+
+(
+
+cd $HOME/kodi
+make -j$(getconf _NPROCESSORS_ONLN) -C tools/depends/target/binary-addons PREFIX=$HOME/kodi.AppDir ADDONS=" audiodecoder.2sf audiodecoder.asap audiodecoder.dumb audiodecoder.fluidsynth audiodecoder.gme audiodecoder.gsf audiodecoder.modplug audiodecoder.ncsf audiodecoder.nosefart audiodecoder.openmpt audiodecoder.organya audiodecoder.qsf audiodecoder.sidplay audiodecoder.snesapu audiodecoder.ssf audiodecoder.stsound audiodecoder.timidity audiodecoder.upse audiodecoder.vgmstream audiodecoder.wsr audiodsp.freesurround audioencoder.flac audioencoder.lame audioencoder.vorbis audioencoder.wav game.libretro imagedecoder.mpo imagedecoder.raw inputstream.adaptive peripheral.joystick pvr.argustvpvr.demo pvr.dvblink pvr.dvbviewer pvr.filmon pvr.hdhomerun pvr.hts pvr.iptvsimple pvr.mediaportal.tvserver pvr.mythtv pvr.nextpvr pvr.njoy pvr.octonet pvr.pctv pvr.sledovanitv.cz pvr.stalker pvr.teleboy pvr.vbox pvr.vdr.vnsi pvr.vupluspvr.wmc pvr.zattoo screensaver.asteroids screensaver.biogenesis screensaver.greynetic screensaver.matrixtrails screensaver.pingpong screensaver.pyro screensaver.stars vfs.libarchive vfs.nfs vfs.rar vfs.sacd vfs.sft pvisualization.goom visualization.shader toyvisualization.spectrum visualization.waveform "
+
+)
+
+
+cd $HOME
+
+cd kodi-build
+wget https://github.com/cmatomic/RecipesAppimage/raw/master/kodi.png
+
+wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/AppRun2
+
+mv AppRun2  AppRun
+
+chmod +x AppRun
+
+wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/kodi.desktop
+
+cd $HOME
 
 wget https://github.com/cmatomic/RecipesAppimage/raw/master/VLCplayerSnap/appimagetool
 
 chmod +x appimagetool
 
 ./appimagetool --no-appstream kodi.AppDir
+
+
