3c64be65fbc443340703c694fcc57f92059a24c3
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-11-11 19:11:35 +0000

    Update Handbrake.sh

--- Handbrake.sh
+++ Handbrake.sh
@@ -1,18 +1,18 @@
 #/bin/sh
-#handbrake-amd64
+#handbrake-amd64 ; for ubuntu xenial
 
 NASM=2.14.02
 GTTEXT=0.20.1
 HDBK=1.2.2
 
 update
-apt-get install autoconf automake build-essential cmake git libass-dev libbz2-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libharfbuzz-dev libjansson-dev liblzma-dev libmp3lame-dev libogg-dev libopus-dev libsamplerate-dev libspeex-dev libtheora-dev libtool libtool-bin libvorbis-dev libx264-dev libxml2-dev m4 make nasm patch pkg-config python tar yasm zlib1g-dev gstreamer1.0-libav intltool libappindicator-dev libdbus-glib-1-dev libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgtk-3-dev libgudev-1.0-dev libnotify-dev libwebkitgtk-3.0-dev meson python3-pip libnuma-dev autopoint doxygen libvpx-dev  libva-dev libvdpau-dev gtk-doc-tools libgirepository1.0-dev libmutter-dev
+apt-get install autoconf automake build-essential cmake git libass-dev libbz2-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libharfbuzz-dev libjansson-dev liblzma-dev libmp3lame-dev libogg-dev libopus-dev libsamplerate-dev libspeex-dev libtheora-dev libtool libtool-bin libvorbis-dev libx264-dev libxml2-dev m4 make nasm patch pkg-config python tar yasm zlib1g-dev gstreamer1.0-libav intltool libappindicator-dev libdbus-glib-1-dev libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgtk-3-dev libgudev-1.0-dev libnotify-dev libwebkitgtk-3.0-dev meson python3-pip libnuma-dev autopoint doxygen libvpx-dev  libva-dev libvdpau-dev gtk-doc-tools libgirepository1.0-dev libmutter-dev --yes
 pip3 install meson
 
 (
   wget https://www.nasm.us/pub/nasm/releasebuilds/$NASM/nasm-$NASM.tar.gz
-  tar xvf nasm-$NASM
-  cd nas-$NASM
+  tar xvf nasm-*
+  cd nas-*
   #./autogen.sh
   ./configure --prefix=/usr 
   make -j$(nproc)
@@ -21,7 +21,7 @@ pip3 install meson
 
 (
   wget https://ftp.gnu.org/pub/gnu/gettext/gettext-$GTTEXT.tar.gz
-  cd gettext-$GTTEXT
+  cd gettext-*
   ./autogen.sh 
   ./configure --prefix=/usr 
   make -j$(nproc)
@@ -39,8 +39,6 @@ pip3 install meson
 
 )
 
-
-
 (
   git clone https://github.com/FFmpeg/FFmpeg.git
   cd FFmpeg 
@@ -54,8 +52,8 @@ pip3 install meson
 
   mkdir HandBrake.AppDir
   wget https://download.handbrake.fr/releases/$HDBK/HandBrake-$HDBK-source.tar.bz2
-  tar xfv HandBrake-$HDBK
-  cd HandBrake-$HDBK
+  tar xfv HandBrake-*
+  cd HandBrake-*
   ./configure --launch-jobs=$(nproc) --launch --build="build" --prefix=/usr --enable-fdk-aac --enable-numa --enable-x265 --debug --arch=x86_64
   make --directory=build install
  
