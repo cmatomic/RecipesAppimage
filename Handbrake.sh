450fbf5a3f6cdea3520cc051ede9bb2f05763a51
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-11-11 19:18:48 +0000

    Update Handbrake.sh

--- Handbrake.sh
+++ Handbrake.sh
@@ -5,7 +5,7 @@ NASM=2.14.02
 GTTEXT=0.20.1
 HDBK=1.2.2
 
-update
+apt update
 apt-get install autoconf automake build-essential cmake git libass-dev libbz2-dev libfontconfig1-dev libfreetype6-dev libfribidi-dev libharfbuzz-dev libjansson-dev liblzma-dev libmp3lame-dev libogg-dev libopus-dev libsamplerate-dev libspeex-dev libtheora-dev libtool libtool-bin libvorbis-dev libx264-dev libxml2-dev m4 make nasm patch pkg-config python tar yasm zlib1g-dev gstreamer1.0-libav intltool libappindicator-dev libdbus-glib-1-dev libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgtk-3-dev libgudev-1.0-dev libnotify-dev libwebkitgtk-3.0-dev meson python3-pip libnuma-dev autopoint doxygen libvpx-dev  libva-dev libvdpau-dev gtk-doc-tools libgirepository1.0-dev libmutter-dev --yes
 pip3 install meson
 
